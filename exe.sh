5524689465dfbda1a8ffa8fc1b9ca005f937c0cb
Alice Sawatzky <alice.sawatzky@farmersedge.ca>
2020-10-27 18:09:15 -0500

    add script for building .exes

---
+++ exe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required env variables:
+# - GOARCH
+# - VERSION
+
+[[ -z "$GOARCH" ]] && echo 'Required variable: $GOARCH' && exit 1
+[[ -z "$VERSION" ]] && echo 'Required variable: $VERSION' && exit 1
+
+GOOS=windows GOARCH="$GOARCH" go build \
+    -ldflags "-X 'github.com/farmersedgeinc/yaml-crypt/cmd.version=$VERSION'" \
+    -o "out/yaml-crypt.$GOARCH.exe"
