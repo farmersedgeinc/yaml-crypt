d982aa84e2683389258573498df14eb2f46bccb8
Alice Sawatzky <alice.sawatzky@farmersedge.ca>
2020-10-29 17:29:38 -0500

    [crypto/providers_test] add tests for crypto providers

---
+++ pkg/crypto/providers_test.go
@@ -0,0 +1,79 @@
+package crypto
+
+import (
+	"testing"
+	"reflect"
+	"github.com/farmersedgeinc/yaml-crypt/pkg/fixtures"
+	"strconv"
+	"golang.org/x/oauth2/google"
+	"context"
+)
+
+type ProviderMeta struct {
+	Provider Provider
+	Skip func() bool
+}
+
+var providers = []ProviderMeta{
+	ProviderMeta{NoopProvider{}, func() bool {return false}},
+	ProviderMeta{
+		GoogleProvider{
+			Project: "yaml-crypt-test-9420f5b24e736f",
+			Location: "global",
+			Keyring: "yamlcrypt-test-49809b3c30a6e22",
+			Key: "yaml-crypt",
+		},
+		func() bool {
+			_, err := google.FindDefaultCredentials(context.Background())
+			return err != nil
+		},
+	},
+}
+
+func TestRoundTrip(t *testing.T) {
+	for _, meta := range providers {
+		provider := meta.Provider
+		name := reflect.TypeOf(provider).Name()
+		t.Run(name, func(t *testing.T) {
+			if meta.Skip() {
+				t.Skip()
+			}
+			for _, original := range fixtures.Strings {
+				ciphertext, err := provider.Encrypt(original)
+				if err != nil {
+					t.Errorf(
+						"Provider %s failed to encrypt with error: %s\nPlaintext: %s",
+						name,
+						strconv.Quote(err.Error()),
+						strconv.Quote(original),
+					)
+				} else if len(ciphertext) == 0 {
+					t.Errorf(
+						"Provider %s produced zero-length ciphertext\nPlaintext: %s",
+						name,
+						strconv.Quote(original),
+					)
+				}
+				plaintext, err := provider.Decrypt(ciphertext)
+				if err != nil {
+					t.Errorf(
+						"Provider %s failed to decrypt with error: %s\nCiphertext: %x\nExpected Plaintext: %s",
+						name,
+						strconv.Quote(err.Error()),
+						ciphertext,
+						strconv.Quote(original),
+					)
+				} else if original != plaintext {
+					t.Errorf(
+						"Round-trip failed for provider %s\nOriginal Plaintext: %s\nCiphertext: %x\nFinal Plaintext: %s",
+						name,
+						strconv.Quote(original),
+						ciphertext,
+						strconv.Quote(plaintext),
+					)
+
+				}
+			}
+		})
+	}
+}
--- pkg/fixtures/fixtures.go
+++ pkg/fixtures/fixtures.go
@@ -60,3 +60,13 @@ type File struct {
 func (f File) Path(r Repo) string {
 	return filepath.Join(r.Dir(), f.Name)
 }
+
+var Strings = []string{
+	"1ZQheycERGjpTeXgJrzpjmxDxaixVJVcstgKyiwUshRx7AwZAsHtRwGVFgDtQlyVRiMMw618Hr4kbty66v1NN7acAnApEBAz9BfwfQ5kz87aGcaPSeck3F8obdyRPHmA",
+	"xQr3HS4TmD87DPLMU17gUicZ",
+	"lWBsiwTmlpA0H5k2nZjz64UM",
+	"test",
+	"{\"message\": \"weird, but could happen\"}",
+	"\n % cat /dev/urandom | strings -n 16\n3 ,V\";u=)gg	H(>{d\nhV9+pprKG	>|=)IyO\nPvico@rXJ{L/g&g\n b83x)))n5GU+oI*a)\n!__dL`;5IX]/)ro1Pa4\nh;{a3U8\\fRYI}07Ivr]\nCy%JJY]kBMl!)tm	\nWo:Y	@1|5<FPFF	t }\n",
+	"🤔🤔🤔",
+}
