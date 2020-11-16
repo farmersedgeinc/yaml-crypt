e89ffe68db340e04dbc11a13da091955e582ffc1
Alice Sawatzky <alice.sawatzky@farmersedge.ca>
2020-11-16 12:42:49 -0600

    [cache] add more comprehensive tests

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -1,9 +1,11 @@
 package cache
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/farmersedgeinc/yaml-crypt/pkg/config"
 	"github.com/farmersedgeinc/yaml-crypt/pkg/fixtures"
+	"strconv"
 	"testing"
 )
 
@@ -29,24 +31,35 @@ func TestCache(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	encryptNonExistent(t, &cache)
-	decryptNonExistent(t, &cache)
-	cache.Close()
+	getItems(t, &cache, 0, false)
+	err = cache.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
 	for round := 0; round < 20; round++ {
 		cache, err := Setup(config)
 		if err != nil {
 			t.Fatal(err)
 		}
-		for item := 0; item < 100; item++ {
-			err := cache.Add(
-				plaintext(round, item),
-				ciphertext(round, item),
-			)
-			if err != nil {
-				t.Fatal(err)
-			}
+		// put this round's items
+		putItems(t, &cache, round)
+		// get this round's and the 2 previous rounds' items
+		for prevRound := round; prevRound >= round-2 && prevRound >= 0; prevRound-- {
+			getItems(t, &cache, round, true)
 		}
-		cache.Close()
+		err = cache.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	cache, err = Setup(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	getItems(t, &cache, 0, false)
+	err = cache.Close()
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
@@ -56,6 +69,52 @@ func plaintext(round, item int) string {
 
 func ciphertext(round, item int) []byte {
 	return []byte(fmt.Sprintf("Ciphertext for round %02d, item %02d", round, item))
+}
+
+func putItems(t *testing.T, cache *Cache, round int) {
+	for item := 0; item < 100; item++ {
+		err := cache.Add(
+			plaintext(round, item),
+			ciphertext(round, item),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func getItems(t *testing.T, cache *Cache, round int, shouldSucceed bool) {
+	for item := 0; item < 100; item++ {
+		ct, ok, err := cache.Encrypt(plaintext(round, item))
+		if err != nil {
+			t.Error(err.Error())
+		}
+		if shouldSucceed && !ok {
+			t.Errorf("Entry not found in cache when encrypting %s", strconv.Quote(plaintext(round, item)))
+		} else if !shouldSucceed && ok {
+			t.Errorf("Entry should not exist, but found in cache when encrypting %s", strconv.Quote(plaintext(round, item)))
+		}
+		if ok && shouldSucceed && !bytes.Equal(ct, ciphertext(round, item)) {
+			t.Errorf("Lookup returned incorrect value when encrypting %s", strconv.Quote(plaintext(round, item)))
+		} else if !shouldSucceed && len(ct) > 0 {
+			t.Errorf("Entry should not exist, but encrypting returned non-empty []bytes when encrypting %s", strconv.Quote(plaintext(round, item)))
+		}
+
+		pt, ok, err := cache.Decrypt(ciphertext(round, item))
+		if err != nil {
+			t.Error(err.Error())
+		}
+		if shouldSucceed && !ok {
+			t.Errorf("Entry not found in cache when decrypting %s", strconv.Quote(string(ciphertext(round, item))))
+		} else if !shouldSucceed && ok {
+			t.Errorf("Entry should not exist, but found in cache when decrypting %s", strconv.Quote(string(ciphertext(round, item))))
+		}
+		if ok && shouldSucceed && pt != plaintext(round, item) {
+			t.Errorf("Lookup returned incorrect value when decrypting %s", strconv.Quote(string(ciphertext(round, item))))
+		} else if !shouldSucceed && len(pt) > 0 {
+			t.Errorf("Entry should not exist, but encrypting returned non-empty []bytes when encrypting %s", strconv.Quote(string(ciphertext(round, item))))
+		}
+	}
 }
 
 func encryptNonExistent(t *testing.T, cache *Cache) {
