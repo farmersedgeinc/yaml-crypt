c54b23fda553afcbefebb833aff4d0cd2638c974
Alice Sawatzky <alice.sawatzky@farmersedge.ca>
2021-03-17 18:28:09 -0500

    [cmd] add encrypt-value command

---
+++ cmd/encrypt_value.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"bufio"
+	"encoding/base64"
+	"fmt"
+	"github.com/farmersedgeinc/yaml-crypt/pkg/actions"
+	"github.com/farmersedgeinc/yaml-crypt/pkg/cache"
+	"github.com/farmersedgeinc/yaml-crypt/pkg/config"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+var encryptValueFlags struct {
+	multiline bool
+}
+
+var EncryptValueCmd = &cobra.Command{
+	Use:                   "encrypt-value",
+	Short:                 "Read in a decrypted value from STDIN and print an encrypted representation to STDOUT",
+	Long:                  "Read in a decrypted value from STDIN and print an encrypted representation to STDOUT. By default it will read in and encrypt a single line.",
+	Args:                  cobra.NoArgs,
+	DisableFlagsInUseLine: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var plaintext string
+		var err error
+		if encryptValueFlags.multiline {
+			rawPlaintext, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				return err
+			}
+			plaintext = string(rawPlaintext)
+		} else {
+			reader := bufio.NewReader(os.Stdin)
+			plaintext, err = reader.ReadString('\n')
+			if err != nil {
+				return err
+			}
+			plaintext = strings.TrimSpace(plaintext)
+		}
+		var ciphertext []byte
+		err = func() error {
+			config, err := config.LoadConfig(".")
+			if err != nil {
+				return err
+			}
+			cache, err := cache.Setup(config)
+			if err != nil {
+				return err
+			}
+			defer cache.Close()
+			ciphertext, err = actions.EncryptPlaintext(string(plaintext), &cache, &config.Provider)
+			return err
+		}()
+		if err != nil {
+			return err
+		}
+		fmt.Println(base64.StdEncoding.EncodeToString(ciphertext) + "\n")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(EncryptValueCmd)
+	EncryptValueCmd.Flags().BoolVarP(&encryptValueFlags.multiline, "multi-line", "m", false, "Read multiple lines of input, stopping only at EOF.")
+}
