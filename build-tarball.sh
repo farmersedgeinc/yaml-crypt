1ae55a9316e942df4751f1ef4c09c6fe300af47d
Alice Sawatzky <alice.sawatzky@farmersedge.ca>
2024-08-19 14:18:48 -0500

    [build-tarball] tidy

--- build-tarball.sh
+++ build-tarball.sh
@@ -15,36 +15,35 @@ set -e
 PKGDIR="$(mktemp -d /tmp/tarball.XXXXXX)"
 trap 'rm -r "$PKGDIR"' EXIT
 
+function pkg_prefix {
+    REL_DIR="${1#/}"
+    if [ "$GOOS" == "darwin" ]; then
+        printf %s/usr/local/%s "$PKGDIR" "${REL_DIR#usr}"
+    else
+        printf %s/%s "$PKGDIR" "$REL_DIR"
+    fi
+}
+
+function pkg_file {
+    PKG_PATH="$(pkg_prefix "$1")"
+    dirname "$PKG_PATH" \
+        | xargs mkdir -p
+    printf -- %s "$PKG_PATH"
+}
+
+function install_completion {
+    GOOS="" GOARCH="" go run main.go completion "$1" > "$(pkg_file "$2")"
+}
+
 # Install Binary
-BINDIR="$PKGDIR/usr/bin"
-mkdir -p "$BINDIR"
-
 GOOS="$GOOS" GOARCH="$GOARCH" go build \
-    -ldflags "-X 'github.com/farmersedgeinc/yaml-crypt/cmd.version=$(echo "$VERSION" | sed 's:^refs/tags/v::g')'" \
-    -o "$BINDIR/yaml-crypt"
+    -ldflags "-X 'github.com/farmersedgeinc/yaml-crypt/cmd.version=${VERSION#refs/tags/v}'" \
+    -o "$(pkg_file /usr/bin/yaml-crypt)"
 
-# Install Bash Completions
-if [[ "$GOOS" -eq "linux" ]]; then
-    BASHFILE="$PKGDIR/etc/bash_completion.d/yaml-crypt"
-else
-    BASHFILE="$PKGDIR/usr/local/etc/bash_completion.d/yaml-crypt"
-fi
-mkdir -p "$(dirname "$BASHFILE")"
-(unset GOOS GOARCH; go run main.go completion bash > "$BASHFILE")
-
-# Install Zsh Completions
-if [[ "$GOOS" -eq "linux" ]]; then
-    ZSHFILE="$PKGDIR/usr/share/zsh/vendor-completions/_yaml-crypt"
-    mkdir -p "$(dirname "$ZSHFILE")"
-    (unset GOOS GOARCH; go run main.go completion zsh > "$ZSHFILE")
-fi
-
-# Install Fish Completions
-if [[ "$GOOS" -eq "linux" ]]; then
-    FISHFILE="$PKGDIR/usr/share/fish/vendor_completions.d/yaml-crypt.fish"
-    mkdir -p "$(dirname "$FISHFILE")"
-    (unset GOOS GOARCH; go run main.go completion fish > "$FISHFILE")
-fi
+# Install Completions
+install_completion bash /etc/bash_completion.d/yaml-crypt
+install_completion zsh /usr/share/zsh/vendor-completions/_yaml-crypt
+install_completion fish /usr/share/fish/vendor_completions.d/yaml-crypt.fish
 
 mkdir -p out
 tar --owner 1 --group 1 -cC "$PKGDIR" "." | gzip -9 > out/yaml-crypt.$GOOS.$GOARCH.tar.gz
